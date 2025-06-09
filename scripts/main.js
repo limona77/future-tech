4bdd2edcce25d4ed78b32a8cdaa029c23ed1da8e
andrey <andrey.artemov0424@gmail.com>
2025-06-09 17:26:39 +0300

    added section blog-details

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 import Header from "./Header.js"
 import TabsCollection from "./Tabs.js";
+import VideoPlayerCollection from "./VideoPlayer.js";
 
 
 new Header()
 new TabsCollection()
+new VideoPlayerCollection()
