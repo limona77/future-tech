278278ff523f66eb409d63554a0b146dc344661e
andrey <andrey.artemov0424@gmail.com>
2025-06-10 17:32:21 +0300

    added section podcasts

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 import Header from "./Header.js"
 import TabsCollection from "./Tabs.js";
 import VideoPlayerCollection from "./VideoPlayer.js";
+import ExpandableContentCollection from "./ExpandableContent.js";
 
 
 new Header()
 new TabsCollection()
 new VideoPlayerCollection()
+new ExpandableContentCollection()
